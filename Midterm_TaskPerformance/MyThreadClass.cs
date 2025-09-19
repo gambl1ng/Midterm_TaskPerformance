b2286051fdaf7a2dcb47721fabdc7d52eb1335e6
gambl1ng <delacruzvivianaira11@gmail.com>
2025-09-19 18:48:57 +0800

    Add project files.

---
+++ Midterm_TaskPerformance/MyThreadClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Midterm_TaskPerformance
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + i);
+                Thread.Sleep(500);
+            }
+        }
+        public static void Thread2()
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + i);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
